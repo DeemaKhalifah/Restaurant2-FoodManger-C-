146b01da470afe2a83bb5273c4a9d19b2047fe5e
Deema Khalifah <deema.khalifah@gmail.com>
2025-07-28 15:15:43 +0200

    Add files via upload

---
+++ EntityFramwork/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Domain;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Data;
+
+
+namespace EntityFramwork
+{
+    public class AppDbContext : DbContext
+
+    {
+        public DbSet<Food> foods { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            var connection = new SqlConnection(config.GetSection("Constr").Value);
+            optionsBuilder.UseSqlServer(connection);
+
+        }
+
+    }
+}
